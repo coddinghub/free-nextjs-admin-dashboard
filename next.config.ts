2bb9fbc4c0f3e7f0603b6808e401be13851c7853
Sam <sam.sys.hk@gmail.com>
2025-04-16 21:41:06 +0800

    add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['tmp.samws.net'],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
