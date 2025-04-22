c120adf9bdaf59cbd1b0e9494436a940b88167b4
Sam <sam.sys.hk@gmail.com>
2025-04-22 17:37:33 +0800

    update domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['tmp.samws.net'],
+  allowedDevOrigins: ['admin.samws.net'],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
