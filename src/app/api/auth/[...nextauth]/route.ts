2fe1944e969b0894fd5710424652490e52f8944f
Sam <sam.sys.hk@gmail.com>
2025-04-23 14:56:00 +0800

    User GoogleProvider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,23 +2,23 @@
 import NextAuth from "next-auth"
 
 // importing providers
-import GithubProvider from "next-auth/providers/github"
-// import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_CLIENT_ID as string,
-        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        //   })
+        // GithubProvider({
+        //     clientId: process.env.GITHUB_ID as string,
+        //     clientSecret: process.env.GITHUB_SECRET as string,
+        // }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+          })
     ],
     callbacks: {
         async signIn({ user}) {
-          if (user.email?.includes("sam.sys.hk@gmail.com")) {
+          if (user.email?.includes(process.env.EMAIL as string)) {
             return true;
           } else {
             return false;
